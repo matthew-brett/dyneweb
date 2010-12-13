d370ddf773640cb0dfe62442c7c686a817e07528
Matthew Brett <matthew.brett@gmail.com>
2010-12-13 14:50:55 -0500

    RF - moved sphinxext from upstream bibstuff move

--- conf.py
+++ conf.py
@@ -25,7 +25,7 @@ sys.path.insert(0, os.path.abspath('sphinxext'))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.pngmath', 'bibref']
+extensions = ['sphinx.ext.pngmath', 'bibstuff.sphinxext.bibref']
 
 # Add custom style for references
 bibref_styles = {'mystyle': 'mystyle'}
